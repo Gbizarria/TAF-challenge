39489a4ea06e7b24cfe746a0c88f192be00ef86b
Gustavo Bizarria <Bizargust@gmail.com>
2023-09-21 15:04:43 -0300

    challenge repair

--- script.js
+++ script.js
@@ -24,26 +24,44 @@ function testTAF(distance, time, gender, pushUps, abs) { //Essa função recebe 
   
     let passed = false;//A variável 'passed' vem por padrão dentro da função como ' false'
     
-      if(gender==="male") //Condições impostas para a aprovação do usuário masculino
-      if(distance>=24000)
-      if(time<=720) 
-      if(pushUps>=30)
-      if(abs>=35)
+      if(gender==="male") {
+      };//Condições impostas para a aprovação do usuário masculino
+        if(distance>=24000){
+
+      };
+        if(time<=720){
+
+          } ;
+          if(pushUps>=30){
+
+          };
+          if(abs>=35){
+
+          };
     
       passed=true // Se os parânmetros da função forem condizentes com as condições impostas, 'passed' se torna 'true'
  
-      if(gender==='female')//Condições impostas para a aprovação do usuário feminino
-      if(distance>=24000)
-      if(time<=840) 
-      if(pushUps>=20)
-      if(abs>=30)
+      if(gender==='female'){
+      };//Condições impostas para a aprovação do usuário feminino
+        if(distance>=24000){
+          
+        };
+          if(time<=840){
+
+          };
+      if(pushUps>=20){
+
+      };
+      if(abs>=30){
+
+      };
         
         passed=true// Se os parânmetros da função forem condizentes com as condições impostas, 'passed' se torna 'true'
    
      
      return passed; // Caso nenhuma das condições impostas seja uma verdade, a variável passa a ter o valor padrão(false)
      
-  }
+  };
    
   console.log( "Candidato passou no teste?: \nresposta: ",
   testTAF(24000,900,'male',19,30),);
