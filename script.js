56243e135dd4c17fe0a9d179cf1d180419e89d22
Gustavo Bizarria <Bizargust@gmail.com>
2023-09-29 01:16:01 -0300

    factoring taf challenge code

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-const distance = 25000;
-const time = 710;
-const gender = "male";
-const pushUps = 40;
-const abs = 35;
-
-
-
-function testTAF(gender,distance,time,pushUps,abs) { //Essa função recebe os parâmetros para executar a condição
- let  passed = false //A variável 'passed' vem por padrão dentro da função como ' false'
-     if(gender==='male'){
-
-     if(distance>=24000)
-
-     if(time<=720)
-
-     if(pushUps>=30)
-
-     if(abs>=35)
-     
-     passed= true
-    };
-     
-     //Condições impostas para a aprovação do usuário feminino
-    if(gender==='female'){
-
-    if(distance>=24000)
-
-
-    if(time<=840)
-
-
-    if(pushUps>=20)
-
-
-    if(abs>=30)
-
-        // Se os parânmetros da função forem condizentes com as condições impostas, 'passed' se torna 'true'
-       passed=true
+//the variables became fuctions that operate the values received.
+function distance(k){
+  if(k>=2400){
+      return true
+    }
 };
-        
-            // Caso nenhuma das condições impostas seja uma verdade, a variável passa a ter o valor padrão(false)
 
-     return passed
-
-
+function time(m){
+  if(m<=720){
+ 
+  return true
 }
-    
+};
 
-  console.log( "Candidato passou no teste?: \nresposta: ",
-  testTAF('female',24000,840,20,30));
-            
+function psuhUps(m){
+  
+  if(m>=30){
+
+    return true
+  }
+};
+
+function abs(ab){
+if(ab>=35){
+
+  return true
+}
+};
+  
+// function tesTAF reuse the functions above to operate your received parameters.
+function testTAF(gender,km,mn,rp,ab){
+
+  if(gender==='male'&&distance(km)&&time(mn)&&psuhUps(rp)&&abs(ab)){
+
+      return true
+
+  }else if(gender==='female'&&distance(km)&&time(mn-120)&&psuhUps(rp+10)&&abs(ab+5)){
+
+      return true
+ 
+  }else
+
+       return false
+  
+  
 
   
-  console.log( "Candidato passou no teste?: \nresposta: ",
-  testTAF('male',24000,720,30,35));
- 
-   
- 
-  module.exports = testTAF;
+      
+  };
+
+//function receives the text and function and returns everything in upper case.
+function showMessagem(text,func){
+ return console.log(`${text}${func}`.toUpperCase())
+};
+
+showMessagem("Candidato passou no teste?: \nresposta: ",
+testTAF('male',24000,720,30,35));
   
+showMessagem( "Candidato passou no teste?: \nresposta: ",
+testTAF('female',24000,840,20,30));
